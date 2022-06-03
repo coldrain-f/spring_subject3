b508491dda00dde4b66d6e4e607655e96a729785
coldrain-f <intmain__@naver.com>
2022-06-03 09:45:19 +0900

    Querydsl 적용 및 적용 테스트

---
+++ src/main/java/edu/coldrain/subject/entity/Hello.java
@@ -0,0 +1,12 @@
+package edu.coldrain.subject.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Hello {
+
+    @Id @GeneratedValue
+    private Long id;
+}
--- src/test/java/edu/coldrain/subject/SpringSubject3ApplicationTests.java
+++ src/test/java/edu/coldrain/subject/SpringSubject3ApplicationTests.java
@@ -1,13 +1,45 @@
 package edu.coldrain.subject;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import edu.coldrain.subject.entity.Hello;
+import edu.coldrain.subject.entity.QHello;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static edu.coldrain.subject.entity.QHello.hello;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class SpringSubject3ApplicationTests {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private JPAQueryFactory query;
+
+    @BeforeEach
+    void before() {
+        query = new JPAQueryFactory(em);
+    }
 
     @Test
     void contextLoads() {
+        Hello h = new Hello();
+        em.persist(h);
+
+        List<Hello> fetch = query.selectFrom(hello)
+                .fetch();
+
+        assertThat(fetch).hasSize(1);
     }
 
 }
