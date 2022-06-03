e4d8eae8942640d4f5c532036e136a5f4ab32a4b
coldrain-f <intmain__@naver.com>
2022-06-03 09:37:52 +0900

    Initial commit

---
+++ src/main/java/edu/coldrain/subject/SpringSubject3Application.java
@@ -0,0 +1,13 @@
+package edu.coldrain.subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSubject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSubject3Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/coldrain/subject/SpringSubject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.coldrain.subject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSubject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
